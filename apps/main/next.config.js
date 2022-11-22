2517303c4c084c4f468e39ccde7434a227434c1e
Thomas Ramé <thomas.rame@outlook.com>
2022-11-22 17:17:57 +0100

    chore(ci): adapt for scalingo

--- apps/main/next.config.js
+++ apps/main/next.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   reactStrictMode: true,
-  output: 'standalone',
+  // output: 'standalone', // This was great to use in case of a Docker image, but it's totally incompatible with Scalingo build pipeline, giving up this size reducing way :D
   experimental: {
     outputFileTracingRoot: path.join(__dirname, '../../'),
     transpilePackages: ['ui'],
