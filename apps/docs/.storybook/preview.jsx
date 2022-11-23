805322b92244774059237a48b93c85daaa0455a6
Thomas Ramé <thomas.rame@outlook.com>
2022-11-23 14:14:30 +0100

    chore(storybook): add missing files

---
+++ apps/docs/.storybook/preview.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import addons from '@storybook/addons';
+import { DocsContainer } from '@storybook/addon-docs';
+import { themes } from '@storybook/theming';
+
+import { DARK_MODE_EVENT_NAME, UPDATE_DARK_MODE_EVENT_NAME } from 'storybook-dark-mode';
+
+const channel = addons.getChannel();
+
+export const parameters = {
+  darkMode: {
+    current: 'light',
+    stylePreview: true,
+    dark: { ...themes.dark },
+    light: { ...themes.light },
+  },
+  docs: {
+    container: (props) => {
+      const [isDark, setDark] = React.useState();
+
+      const onChangeHandler = () => {
+        channel.emit(UPDATE_DARK_MODE_EVENT_NAME);
+      };
+
+      React.useEffect(() => {
+        channel.on(DARK_MODE_EVENT_NAME, setDark);
+        return () => channel.removeListener(DARK_MODE_EVENT_NAME, setDark);
+      }, [channel, setDark]);
+
+      return (
+        <div>
+          <input type="checkbox" onChange={onChangeHandler} />
+          <DocsContainer {...props} />
+        </div>
+      );
+    },
+  },
+};
