4c54275eaace6636a1ead68de766a22db2399397
Thomas Ramé <thomas.rame@outlook.com>
2022-11-28 18:25:43 +0100

    chore(jest): make able to use jest with typescript

--- apps/main/app/page.spec.tsx
+++ apps/main/app/page.spec.tsx
@@ -1,5 +1,7 @@
 import '@testing-library/jest-dom';
+import '@testing-library/jest-dom/extend-expect';
 import { render, screen } from '@testing-library/react';
+import React from 'react';
 
 import Home from './page';
 
--- apps/main/jest.config.ts
+++ apps/main/jest.config.ts
@@ -6,8 +6,20 @@ const createJestConfig = nextJest({
 
 // Add any custom config to be passed to Jest
 const customJestConfig = {
+  setupFilesAfterEnv: [],
   moduleDirectories: ['node_modules', '<rootDir>/'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+  },
   testEnvironment: 'jest-environment-jsdom',
+  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+  globals: {
+    'ts-jest': {
+      tsconfig: {
+        jsx: 'react',
+      },
+    },
+  },
 };
 
 export default createJestConfig(customJestConfig);
