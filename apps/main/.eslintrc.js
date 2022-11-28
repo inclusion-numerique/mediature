23648df69f215ae60224d19df043cbffe83bdcd7
Thomas Ramé <thomas.rame@outlook.com>
2022-11-28 18:32:30 +0100

    chore(jest): bind it to eslint

--- apps/main/.eslintrc.js
+++ apps/main/.eslintrc.js
@@ -1,4 +1,12 @@
 module.exports = {
   root: true,
   extends: ['custom'],
+  plugins: ['testing-library'],
+  overrides: [
+    // Only uses Testing Library lint rules in test files
+    {
+      files: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
+      extends: ['plugin:testing-library/react'],
+    },
+  ],
 };
