acc41c740613055845993794d49497f5673c9305
Thomas Ramé <thomas.rame@outlook.com>
2022-12-05 11:55:07 +0100

    wip

--- apps/main/app/layout.tsx
+++ apps/main/app/layout.tsx
@@ -1,5 +1,15 @@
 import getConfig from 'next/config';
 
+// TODO: enable Axe for React once compatible
+// Ref: https://github.com/dequelabs/axe-core-npm/issues/500#issuecomment-1337125964
+// // Enable Axe
+// if (typeof window !== 'undefined' && process.env.NODE_ENV !== 'production') {
+//   const React = require('react');
+//   const ReactDOM = require('react-dom');
+//   const axe = require('@axe-core/react');
+//   axe(React, ReactDOM, 1000);
+// }
+
 export default function RootLayout({
   // Layouts must accept a children prop.
   // This will be populated with nested layouts or pages
