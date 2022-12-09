1e8f588de1b81a6e11bd336a93ce4edfcfb9def4
Thomas Ramé <thomas.rame@outlook.com>
2022-12-09 11:20:14 +0100

    chore: allow debugging forms

--- apps/main/app/(public)/request/[authority]/RequestCaseForm.tsx
+++ apps/main/app/(public)/request/[authority]/RequestCaseForm.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { DevTool } from '@hookform/devtools';
 import { zodResolver } from '@hookform/resolvers/zod';
 import {
   Button,
@@ -27,6 +28,7 @@ export function RequestCaseForm() {
     register,
     handleSubmit,
     formState: { errors },
+    control,
   } = useForm<RequestCaseSchemaType>({
     resolver: zodResolver(RequestCaseSchema),
   });
@@ -36,142 +38,146 @@ export function RequestCaseForm() {
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <Grid container>
-        <Grid item xs={12} sm={6}>
-          {/* <TextField name="firstname" label="Prénom" placeholder="ex: Marie" fullWidth InputLabelProps={{ shrink: true }} /> */}
-          <TextField
-            name="firstname"
-            label="Prénom"
-            placeholder="ex: Marie"
-            {...register('firstname')}
-            error={!!errors.firstname}
-            helperText={errors?.firstname?.message}
-            fullWidth
-          />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <TextField
-            name="lastname"
-            label="Nom"
-            placeholder="ex: Dupont"
-            {...register('lastname')}
-            error={!!errors.lastname}
-            helperText={errors?.lastname?.message}
-            fullWidth
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <TextField
-            name="address"
-            label="Adresse"
-            placeholder="ex: 20 rue de la ..."
-            {...register('address')}
-            error={!!errors.address}
-            helperText={errors?.address?.message}
-            fullWidth
-          />
-          {/* TODO: split into multiple components */}
-        </Grid>
-        <Grid item xs={12}>
-          <TextField
-            name="phone"
-            label="Téléphone"
-            placeholder="ex: 06 66 66 66 66"
-            {...register('phone')}
-            error={!!errors.phone}
-            helperText={errors?.phone?.message}
-            fullWidth
-          />
-          {/* TODO: split into multiple components */}
-        </Grid>
-        <Grid item xs={12}>
-          <TextField
-            type="email"
-            name="email"
-            label="Email"
-            placeholder="ex: marie.dupont@mail.com"
-            {...register('email')}
-            error={!!errors.email}
-            helperText={errors?.email?.message}
-            fullWidth
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <FormControl error={!!errors.alreadyRequestedInThePast}>
-            <FormHelperText id="previous-request-helper-text">
-              Pour que nous puissions au mieux traiter votre demande, veuillez répondre à la question suivante. Nous prendrons en compte votre demande
-              peu importe votre réponse.
-            </FormHelperText>
-            <FormLabel id="previous-request-radio-buttons-group-label">Avez-vous effectué un premier recours à l&apos;amiable ?</FormLabel>
-            <RadioGroup
-              name="previous-request-radio-buttons-group"
-              {...register('alreadyRequestedInThePast')}
-              aria-labelledby="previous-request-radio-buttons-group-label"
-              aria-describedby="previous-request-helper-text"
-            >
-              <FormControlLabel value="yes" control={<Radio />} label="Oui, j'ai effectué un premier recours à l'amiable" />
-              <FormControlLabel value="no" control={<Radio />} label="Non, je n'ai pas effectué de premier recours à l'amiable" />
-            </RadioGroup>
-            <FormHelperText>{errors?.alreadyRequestedInThePast?.message}</FormHelperText>
-          </FormControl>
-        </Grid>
-        <Grid item xs={12}>
-          <FormControl error={!!errors.gotAnswerFromPreviousRequest}>
-            {/* TODO: if above */}
-            <FormLabel id="answer-from-previous-request--radio-buttons-group-label">
-              Suite à ce premier recours à l&apos;amiable, avez-vous reçu une réponse de la part de l&apos;organisme à la charge de votre demande ?
-            </FormLabel>
-            <RadioGroup
-              name="answer-from-previous-request-radio-buttons-group"
-              {...register('gotAnswerFromPreviousRequest')}
-              aria-labelledby="answer-from-previous-request--radio-buttons-group-label"
-            >
-              <FormControlLabel value="yes" control={<Radio />} label="Oui, j'ai obtenu une réponse" />
-              <FormControlLabel value="no" control={<Radio />} label="Non, je n'ai pas obtenu de réponse" />
-            </RadioGroup>
-            <FormHelperText>{errors?.gotAnswerFromPreviousRequest?.message}</FormHelperText>
-          </FormControl>
-        </Grid>
-        <Grid item xs={12}>
-          <TextField
-            name="description"
-            label="Motif de la demande :"
-            {...register('description')}
-            error={!!errors.description}
-            helperText={errors?.description?.message}
-            multiline
-            rows={3}
-            fullWidth
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <FormControl>
-            <FormHelperText id="upload-helper-text">
-              Si vous avez des documents susceptibles de nous aider, merci de les envoyer en cliquant sur le bouton ci-dessous :{' '}
-            </FormHelperText>
-            <Button variant="contained" component="label" aria-describedby="upload-helper-text">
-              {/* TODO: improve + extensions */}
-              Joindre un fichier
-              <input hidden accept="image/*" multiple type="file" />
-            </Button>
-          </FormControl>
-        </Grid>
-        <Grid item xs={12}>
-          <FormControl error={!!errors.emailCopyWanted}>
-            <FormControlLabel
-              label="Envoyez-moi par e-mail une copie de mes réponses."
-              control={<Checkbox name="" {...register('emailCopyWanted')} />}
+    <>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Grid container>
+          <Grid item xs={12} sm={6}>
+            {/* <TextField name="firstname" label="Prénom" placeholder="ex: Marie" fullWidth InputLabelProps={{ shrink: true }} /> */}
+            <TextField
+              name="firstname"
+              label="Prénom"
+              placeholder="ex: Marie"
+              {...register('firstname')}
+              error={!!errors.firstname}
+              helperText={errors?.firstname?.message}
+              fullWidth
             />
-            <FormHelperText>{errors?.emailCopyWanted?.message}</FormHelperText>
-          </FormControl>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              name="lastname"
+              label="Nom"
+              placeholder="ex: Dupont"
+              {...register('lastname')}
+              error={!!errors.lastname}
+              helperText={errors?.lastname?.message}
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              name="address"
+              label="Adresse"
+              placeholder="ex: 20 rue de la ..."
+              {...register('address')}
+              error={!!errors.address}
+              helperText={errors?.address?.message}
+              fullWidth
+            />
+            {/* TODO: split into multiple components */}
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              name="phone"
+              label="Téléphone"
+              placeholder="ex: 06 66 66 66 66"
+              {...register('phone')}
+              error={!!errors.phone}
+              helperText={errors?.phone?.message}
+              fullWidth
+            />
+            {/* TODO: split into multiple components */}
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              type="email"
+              name="email"
+              label="Email"
+              placeholder="ex: marie.dupont@mail.com"
+              {...register('email')}
+              error={!!errors.email}
+              helperText={errors?.email?.message}
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <FormControl error={!!errors.alreadyRequestedInThePast}>
+              <FormHelperText id="previous-request-helper-text">
+                Pour que nous puissions au mieux traiter votre demande, veuillez répondre à la question suivante. Nous prendrons en compte votre
+                demande peu importe votre réponse.
+              </FormHelperText>
+              <FormLabel id="previous-request-radio-buttons-group-label">Avez-vous effectué un premier recours à l&apos;amiable ?</FormLabel>
+              <RadioGroup
+                name="previous-request-radio-buttons-group"
+                {...register('alreadyRequestedInThePast')}
+                aria-labelledby="previous-request-radio-buttons-group-label"
+                aria-describedby="previous-request-helper-text"
+              >
+                <FormControlLabel value={true} control={<Radio />} label="Oui, j'ai effectué un premier recours à l'amiable" />
+                <FormControlLabel value={false} control={<Radio />} label="Non, je n'ai pas effectué de premier recours à l'amiable" />
+              </RadioGroup>
+              <FormHelperText>{errors?.alreadyRequestedInThePast?.message}</FormHelperText>
+            </FormControl>
+          </Grid>
+          <Grid item xs={12}>
+            <FormControl error={!!errors.gotAnswerFromPreviousRequest}>
+              {/* TODO: if above */}
+              <FormLabel id="answer-from-previous-request--radio-buttons-group-label">
+                Suite à ce premier recours à l&apos;amiable, avez-vous reçu une réponse de la part de l&apos;organisme à la charge de votre demande ?
+              </FormLabel>
+              <RadioGroup
+                name="answer-from-previous-request-radio-buttons-group"
+                {...register('gotAnswerFromPreviousRequest')}
+                aria-labelledby="answer-from-previous-request--radio-buttons-group-label"
+              >
+                <FormControlLabel value={true} control={<Radio />} label="Oui, j'ai obtenu une réponse" />
+                <FormControlLabel value={false} control={<Radio />} label="Non, je n'ai pas obtenu de réponse" />
+              </RadioGroup>
+              <FormHelperText>{errors?.gotAnswerFromPreviousRequest?.message}</FormHelperText>
+            </FormControl>
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              name="description"
+              label="Motif de la demande :"
+              {...register('description')}
+              error={!!errors.description}
+              helperText={errors?.description?.message}
+              multiline
+              rows={3}
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <FormControl>
+              <FormHelperText id="upload-helper-text">
+                Si vous avez des documents susceptibles de nous aider, merci de les envoyer en cliquant sur le bouton ci-dessous :{' '}
+              </FormHelperText>
+              <Button variant="contained" component="label" aria-describedby="upload-helper-text">
+                {/* TODO: improve + extensions */}
+                Joindre un fichier
+                <input hidden accept="image/*" multiple type="file" />
+              </Button>
+            </FormControl>
+          </Grid>
+          <Grid item xs={12}>
+            <FormControl error={!!errors.emailCopyWanted}>
+              <FormControlLabel
+                label="Envoyez-moi par e-mail une copie de mes réponses."
+                control={<Checkbox name="" {...register('emailCopyWanted')} />}
+              />
+              <FormHelperText>{errors?.emailCopyWanted?.message}</FormHelperText>
+            </FormControl>
+          </Grid>
+          <Grid item xs={12}>
+            <FormControl>
+              <Button type="submit">Envoyer</Button>
+            </FormControl>
+          </Grid>
         </Grid>
-        <Grid item xs={12}>
-          <FormControl>
-            <Button type="submit">Envoyer</Button>
-          </FormControl>
-        </Grid>
-      </Grid>
-    </form>
+      </form>
+
+      <DevTool control={control} />
+    </>
   );
 }
