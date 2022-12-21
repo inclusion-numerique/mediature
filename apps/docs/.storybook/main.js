ba52c4e6230d997c7f8e59d3380694bedc38c4cb
Thomas Ramé <thomas.rame@outlook.com>
2022-12-21 13:10:50 +0100

    chore(sb): clean all "vite" stuff and adapt to webpack configuration

--- apps/docs/.storybook/main.js
+++ apps/docs/.storybook/main.js
@@ -1,7 +1,5 @@
 const fg = require('fast-glob');
 const path = require('path');
-const { mergeConfig } = require('vite');
-const { default: tsconfigPaths } = require('vite-tsconfig-paths');
 
 const getStories = () =>
   fg.sync([
@@ -47,52 +45,26 @@ module.exports = {
   env: (config) => ({
     ...config,
     ENABLE_MOCKS: 'true',
+    STORYBOOK_ENVIRONMENT: 'true',
     TRPC_SERVER_MOCK: 'true',
   }),
-  async viteFinal(config, { configType }) {
-    // return mergeConfig(config, {
-    //   plugins: [
-    //     tsconfigPaths({
-    //       projects: [path.resolve(__dirname, '../../../packages/tsconfig/base.json')],
-    //     }),
-    //   ],
-    // });
+  async webpackFinal(config, { configType }) {
+    config.module.rules = [...config.module.rules];
 
-    // TODO: the above is supposed to work... but it's not, so hardcoding paths for now
-    return mergeConfig(config, {
-      module: {
-        rules: [
-          {
-            test: /\.(css)$/i,
-            use: ['style-loader', 'css-loader'],
-          },
-        ],
-      },
-      resolve: {
-        alias: [
-          {
-            find: 'path',
-            replacement: 'path-browserify',
-          },
-          {
-            find: '@mediature/docs',
-            replacement: path.resolve(__dirname, '../../../apps/docs/'),
-          },
-          {
-            find: '@mediature/main',
-            replacement: path.resolve(__dirname, '../../../apps/main/'),
-          },
-          {
-            find: '@mediature/ui',
-            replacement: path.resolve(__dirname, '../../../packages/ui/'),
-          },
-          {
-            find: '@trpc/next-layout',
-            replacement: path.resolve(__dirname, '../../../packages/trpc-next-layout/'),
-          },
-        ],
-      },
-    });
+    if (!config.resolve) {
+      config.resolve = { alias: {} };
+    }
+
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      path$: 'path-browserify',
+      '@mediature/docs': '../../../apps/docs/',
+      '@mediature/main': '../../../apps/main/',
+      '@mediature/ui': '../../../packages/ui/',
+      '@trpc/next-layout': '../../../packages/trpc-next-layout/',
+    };
+
+    return config;
   },
   docs: {
     docsPage: 'automatic',
