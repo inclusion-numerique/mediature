0dd0754574f746e29143f19747afe4beaf2ef133
Thomas Ramé <thomas.rame@outlook.com>
2022-12-21 23:25:14 +0100

    patch(sb): disable snapshots on emails components since we have a rerender issue that breaks tests

---
+++ packages/ui/src/emails/storybook-utils.ts
@@ -0,0 +1,5 @@
+export const commonEmailsParameters = {
+  // TODO: for now we have a race condition on rendering it the first time so it breaks tests into Chromatic,
+  // we disable the snapshots for now until we find a solution
+  chromatic: { disableSnapshot: true },
+};
