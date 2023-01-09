4f81c3425a7755ccdaa63e71544e36775df16083
Thomas Ramé <thomas.rame@outlook.com>
2023-01-09 16:55:17 +0100

    fix(mock): make the base url adjusted depending on the location protocol

--- apps/main/src/server/mock/environment.ts
+++ apps/main/src/server/mock/environment.ts
@@ -2,4 +2,5 @@ export function shouldTargetMock() {
   return process.env.TRPC_SERVER_MOCK === 'true';
 }
 
-export const mockBaseUrl = 'http://mock.local';
+// Adjust the protocol so it's not blocked due to an "insecure source"
+export const mockBaseUrl = `${typeof window !== 'undefined' ? window.location.protocol : 'http:'}//mock.local`;
