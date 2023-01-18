3522fe677a26f47889826016d3933f55d257b909
Thomas Ramé <thomas.rame@outlook.com>
2023-01-18 10:32:47 +0100

    fix(sb): prevent context leak between stories of the same file

--- apps/docs/.storybook/test-runner.js
+++ apps/docs/.storybook/test-runner.js
@@ -1,5 +1,11 @@
+const { getStoryContext } = require('@storybook/test-runner');
+
 module.exports = {
   setup() {},
-  async preRender(page, context) {},
+  async preRender(page, context) {
+    // [WORKAROUND] To avoid context leak between stories of the same file, it's needed to wait for the new story to be ready
+    // Ref: https://github.com/storybookjs/test-runner/issues/244
+    await getStoryContext(page, context);
+  },
   async postRender(page, context) {},
 };
