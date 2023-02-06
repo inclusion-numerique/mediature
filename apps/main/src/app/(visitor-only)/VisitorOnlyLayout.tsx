2e0b96c5270f1d5969898b39dbfd95cd60f0bf25
Thomas Ramé <thomas.rame@outlook.com>
2023-02-06 09:59:40 +0100

    chore: add linkRegistry at missing places

--- apps/main/src/app/(visitor-only)/VisitorOnlyLayout.tsx
+++ apps/main/src/app/(visitor-only)/VisitorOnlyLayout.tsx
@@ -5,6 +5,7 @@ import { PropsWithChildren, useEffect } from 'react';
 
 import { PublicLayout } from '@mediature/main/src/app/(public)/PublicLayout';
 import { useSession } from '@mediature/main/src/proxies/next-auth/react';
+import { linkRegistry } from '@mediature/main/src/utils/routes/registry';
 import { LoadingArea } from '@mediature/ui/src/LoadingArea';
 
 export function VisitorOnlyLayout(props: PropsWithChildren) {
@@ -16,7 +17,7 @@ export function VisitorOnlyLayout(props: PropsWithChildren) {
       // TODO: this shows the children during a few ms... maybe the condition should be inside the useEffect too?
       // or find a better hook than triggers ASAP without making React throwing error because it's currently rendering
       // ... for now I just fixed it by forcing skeleton also when "authenticated" is true in the re-render
-      router.push('/dashboard');
+      router.push(linkRegistry.get('dashboard', undefined));
     }
   }, [router, sessionWrapper.status]);
 
