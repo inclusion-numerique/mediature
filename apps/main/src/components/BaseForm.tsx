8680617cb8ea739637b0379e7ca74fa2823186a6
Thomas Ramé <thomas.rame@outlook.com>
2023-02-07 10:43:15 +0100

    chore: make BaseForm displaying API errors

--- apps/main/src/components/BaseForm.tsx
+++ apps/main/src/components/BaseForm.tsx
@@ -4,7 +4,8 @@ import { FormEventHandler, PropsWithChildren, useState } from 'react';
 import { Control, FieldErrorsImpl, FieldValues, UseFormHandleSubmit } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
 
-import { stopSubmitPropagation } from '../utils/form';
+import { stopSubmitPropagation } from '@mediature/main/src/utils/form';
+import { ErrorAlert } from '@mediature/ui/src/ErrorAlert';
 
 export interface BaseFormProps<FormSchemaType extends FieldValues> {
   handleSubmit: UseFormHandleSubmit<FormSchemaType>;
@@ -17,13 +18,28 @@ export interface BaseFormProps<FormSchemaType extends FieldValues> {
 // When you want to debug a form, just uncomment the below line (I did not see the value to manage it through environment variable)
 export function BaseForm<FormSchemaType extends FieldValues>(props: PropsWithChildren<BaseFormProps<FormSchemaType>>) {
   const { t } = useTranslation('common');
-  const [errors, setErrors] = useState<Partial<FieldErrorsImpl<any>>>(props.control._formState.errors);
+  const [validationErrors, setValidationErrors] = useState<Partial<FieldErrorsImpl<any>>>(props.control._formState.errors);
+  const [onSubmitError, setOnSubmitError] = useState<Error | null>(null);
 
   // Some forms in dialogs need to force stopping submit propagation leakage on parent forms
   const onSubmit: FormEventHandler<HTMLFormElement> = (...args) => {
-    const submitChain = props.handleSubmit(props.onSubmit, (errors) => {
-      setErrors(errors);
-    });
+    const submitChain = props.handleSubmit(
+      async (input: FormSchemaType) => {
+        try {
+          await props.onSubmit(input);
+        } catch (err: unknown) {
+          if (err instanceof Error) {
+            setOnSubmitError(err);
+          } else {
+            setOnSubmitError(err as any); // The default case is good enough for now
+          }
+        }
+      },
+      (errors) => {
+        // Only triggered on inputs validation errors
+        setValidationErrors(errors);
+      }
+    );
 
     (props.preventParentFormTrigger ? stopSubmitPropagation(submitChain) : submitChain).apply(null, args);
   };
@@ -34,9 +50,15 @@ export function BaseForm<FormSchemaType extends FieldValues>(props: PropsWithChi
 
       <form onSubmit={onSubmit} aria-label={props.ariaLabel}>
         <Grid container spacing={2}>
-          {Object.keys(errors).length > 0 && (
+          {!!onSubmitError && (
             <Grid item xs={12} sx={{ py: 2 }}>
-              <Alert severity="error">{t('components.BaseForm.form_contains_errors', { count: Object.keys(errors).length })}</Alert>
+              <ErrorAlert errors={[onSubmitError]} />
+            </Grid>
+          )}
+
+          {Object.keys(validationErrors).length > 0 && (
+            <Grid item xs={12} sx={{ py: 2 }}>
+              <Alert severity="error">{t('components.BaseForm.form_contains_errors', { count: Object.keys(validationErrors).length })}</Alert>
             </Grid>
           )}
 
