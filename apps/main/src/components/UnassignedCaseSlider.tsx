48e7d9ef884df4b43c2659501f5889467afbc7ac
Thomas Ramé <thomas.rame@outlook.com>
2023-05-05 17:38:38 +0200

    chore: improve slider ux with navigation arrows

--- apps/main/src/components/UnassignedCaseSlider.tsx
+++ apps/main/src/components/UnassignedCaseSlider.tsx
@@ -1,5 +1,9 @@
+import ArrowCircleLeftIcon from '@mui/icons-material/ArrowCircleLeft';
+import ArrowCircleRightIcon from '@mui/icons-material/ArrowCircleRight';
 import Grid from '@mui/material/Grid';
-import { A11y, Keyboard, Mousewheel, Scrollbar } from 'swiper';
+import IconButton from '@mui/material/IconButton';
+import { useRef, useState } from 'react';
+import { A11y, Keyboard, Mousewheel, Navigation, Swiper as SwiperType } from 'swiper';
 import 'swiper/css';
 import 'swiper/css/scrollbar';
 import { Swiper, SwiperSlide } from 'swiper/react';
@@ -19,6 +23,10 @@ export interface UnassignedCaseSliderProps {
 }
 
 export function UnassignedCaseSlider({ authorityId, assignAction }: UnassignedCaseSliderProps) {
+  const swiperRef = useRef<SwiperType>();
+  const [isBeginning, setIsBeginning] = useState<boolean>(false);
+  const [isEnd, setIsEnd] = useState<boolean>(false);
+
   const { data, error, isInitialLoading, isLoading, refetch } = trpc.listCases.useQuery({
     orderBy: {},
     filterBy: {
@@ -74,16 +82,22 @@ export function UnassignedCaseSlider({ authorityId, assignAction }: UnassignedCa
         a11y={{
           enabled: true,
         }}
-        scrollbar={{
-          enabled: false,
-          draggable: true,
-        }}
         mousewheel={false}
         keyboard={{
           enabled: true,
         }}
-        modules={[A11y, Scrollbar, Mousewheel, Keyboard]}
+        modules={[A11y, Mousewheel, Keyboard, Navigation]}
         className={styles.swiper}
+        onBeforeInit={(swiper) => {
+          swiperRef.current = swiper;
+
+          setIsBeginning(swiper.isBeginning);
+          setIsEnd(swiper.isEnd);
+        }}
+        onSlideChange={(swiper) => {
+          setIsBeginning(swiper.isBeginning);
+          setIsEnd(swiper.isEnd);
+        }}
       >
         {casesWrappers.map((caseWrapper) => (
           <SwiperSlide key={caseWrapper.case.id} role="group" aria-roledescription="élément du carrousel" className={styles.swiperSlide}>
@@ -100,6 +114,36 @@ export function UnassignedCaseSlider({ authorityId, assignAction }: UnassignedCa
             />
           </SwiperSlide>
         ))}
+        <IconButton
+          onClick={() => swiperRef.current?.slidePrev()}
+          color="primary"
+          size="large"
+          disabled={isBeginning}
+          aria-label="aller au dossier précédent"
+          sx={{
+            position: 'absolute',
+            bottom: '50%',
+            left: 0,
+            zIndex: 100,
+          }}
+        >
+          <ArrowCircleLeftIcon fontSize="inherit" />
+        </IconButton>
+        <IconButton
+          onClick={() => swiperRef.current?.slideNext()}
+          color="primary"
+          size="large"
+          disabled={isEnd}
+          aria-label="aller au dossier suivant"
+          sx={{
+            position: 'absolute',
+            bottom: '50%',
+            right: 0,
+            zIndex: 100,
+          }}
+        >
+          <ArrowCircleRightIcon fontSize="inherit" />
+        </IconButton>
       </Swiper>
     </>
   );
