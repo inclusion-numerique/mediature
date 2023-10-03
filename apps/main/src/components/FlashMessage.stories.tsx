8f3c2f597b09986e46c2fee03fb970a7d1f5278f
Thomas Ramé <thomas.rame@outlook.com>
2023-10-03 21:30:01 +0200

    chore: notify people if currently on the deployed dev environment

---
+++ apps/main/src/components/FlashMessage.stories.tsx
@@ -0,0 +1,41 @@
+import List from '@mui/material/List';
+import { Meta, StoryFn } from '@storybook/react';
+import { within } from '@storybook/testing-library';
+
+import { StoryHelperFactory } from '@mediature/docs/.storybook/helpers';
+import { FlashMessage } from '@mediature/main/src/components/FlashMessage';
+import { uiAttachments } from '@mediature/main/src/fixtures/attachment';
+
+type ComponentType = typeof FlashMessage;
+const { generateMetaDefault, prepareStory } = StoryHelperFactory<ComponentType>();
+
+export default {
+  title: 'Components/FlashMessage',
+  component: FlashMessage,
+  ...generateMetaDefault({
+    parameters: {},
+  }),
+} as Meta<ComponentType>;
+
+const Template: StoryFn<ComponentType> = (args) => {
+  return <FlashMessage {...args} />;
+};
+
+const DevelopmentEnvStory = Template.bind({});
+DevelopmentEnvStory.args = {
+  appMode: 'dev',
+  nodeEnv: 'production',
+};
+DevelopmentEnvStory.play = async ({ canvasElement }) => {
+  await within(canvasElement).findByText(/version/i);
+};
+
+export const DevelopmentEnv = prepareStory(DevelopmentEnvStory);
+
+const ProductionEnvStory = Template.bind({});
+ProductionEnvStory.args = {
+  appMode: 'prod',
+  nodeEnv: 'production',
+};
+
+export const ProductionEnv = prepareStory(ProductionEnvStory);
