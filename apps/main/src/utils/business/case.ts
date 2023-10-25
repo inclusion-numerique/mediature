cf129a98d2dac957d3fffffb29912cc8a4cea5ed
Thomas Ramé <thomas.rame@outlook.com>
2023-10-26 00:01:31 +0200

    fix(types): `TFunction` was leaking from i18next once imported indirectly from a .tsx, causing OOM for `tsc`

--- apps/main/src/utils/business/case.ts
+++ apps/main/src/utils/business/case.ts
@@ -1,9 +1,16 @@
-import { TFunction } from 'i18next';
-
 import { getServerTranslation, supportedLanguages } from '@mediature/main/src/i18n';
 
+// [WORKAROUND] `TFunction` is leaking into React types as soon as I import this current file into a `.tsx` file
+// Casting as `any` is fine here due to single determined usage. There are no solution for now as stated into:
+// - https://github.com/microsoft/TypeScript/issues/53087
+// - https://github.com/i18next/next-i18next/issues/1795
+// ---
+// import { TFunction } from 'i18next';
+// type TFunc = TFunction<'common'[], undefined, 'common'[];
+type TFunc = any;
+
 export function getCaseEmailFactory(domain: string) {
-  return function getCaseEmail(t: TFunction<'common'[], undefined, 'common'[]>, humanCaseId: string): string {
+  return function getCaseEmail(t: TFunc, humanCaseId: string): string {
     return `${t('model.case.technicalName', { humanId: humanCaseId.toString() })}@${domain}`;
   };
 }
