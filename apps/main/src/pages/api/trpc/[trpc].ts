d030e65dacdf7fa6b4694d0cf901b767adb89b7b
Thomas Ramé <thomas.rame@outlook.com>
2023-10-28 00:19:02 +0200

    chore(trpc): report error to sentry if unexpected

--- apps/main/src/pages/api/trpc/[trpc].ts
+++ apps/main/src/pages/api/trpc/[trpc].ts
@@ -1,5 +1,7 @@
+import * as Sentry from '@sentry/nextjs';
 import { createNextApiHandler } from '@trpc/server/adapters/next';
 
+import { BusinessError } from '@mediature/main/src/models/entities/errors';
 import { appRouter } from '@mediature/main/src/server/app-router';
 import { createContext } from '@mediature/main/src/server/context';
 import { apiHandlerWrapper } from '@mediature/main/src/utils/api';
@@ -12,10 +14,20 @@ export const handler = createNextApiHandler({
       ...opts,
     });
   },
-  onError({ error }) {
-    if (error.code === 'INTERNAL_SERVER_ERROR') {
-      // send to bug reporting
-      console.error('Something went wrong', error);
+  onError({ error, input, ctx }) {
+    // This is called before the tRPC `errorFormatter` middleware
+    // ---
+    // Note about `error.code`:
+    // - `BAD_REQUEST` is used if there is a failure inside input validation (zod in our case), they can be considered as business errors and don't need to be reported
+    // - `INTERNAL_SERVER_ERROR` from within routes
+
+    if (error.code === 'INTERNAL_SERVER_ERROR' && !(error.cause instanceof BusinessError)) {
+      // Notify Sentry of this unexpected error (since handler wrapper will not see it)
+      Sentry.withScope(function (scope) {
+        scope.setUser(ctx?.user ? { id: ctx.user.id } : null);
+
+        Sentry.captureException(error);
+      });
     }
   },
   batching: {
