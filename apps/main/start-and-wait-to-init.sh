4c431dfafe787ff8886e4123288a05dcae62e6f8
Thomas Ramé <thomas.rame@outlook.com>
2025-03-25 09:44:31 +0100

    fix(scalingo): the application is no longer reachable from localhost for healthcheck

--- apps/main/start-and-wait-to-init.sh
+++ apps/main/start-and-wait-to-init.sh
@@ -1,11 +1,11 @@
-#!/bin/bash
+#!/bin/sh
 
 #
 # We trigger an init endpoint to set up all async processes like cron scheduling
 # because unfortunately Next.js does not provide a proper way to use a startup callback
 #
 
-# Start the server in background
+echo "Start the server in background"
 node ./dist/apps/main/server.js &
 
 # Store the server process ID
@@ -18,14 +18,17 @@ stop_server() {
 }
 
 # Bind the callback to the SIGINT signal to shut down the background process properly
-trap stop_server SIGINT
+trap stop_server INT
 
 check_server_and_init() {
   timeout=15
   counter=0
 
+  host="$HOSTNAME:$PORT"
+  healthcheck_url="http://$host"
+
   while true; do
-    response=$(curl --write-out %{http_code} --silent --output /dev/null http://localhost:$PORT)
+    response=$(curl --write-out %{http_code} --silent --output /dev/null "$healthcheck_url")
     if [ "$response" = "200" ]; then
       break
     fi
@@ -38,11 +41,13 @@ check_server_and_init() {
       exit 1
     fi
 
+    echo "the Next.js server is not yet ready on $healthcheck_url"
+
     sleep 1
     counter=$((counter+1))
   done
 
-  curl http://localhost:$PORT/api/init
+  curl http://$host/api/init
 }
 
 # In parallel wait for the server readiness to init some services
@@ -50,3 +55,5 @@ check_server_and_init
 
 # Wait for the Next.js server to return
 wait
+
+echo "The wait instruction has exited normally"
